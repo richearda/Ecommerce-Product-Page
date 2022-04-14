352c4226cb681b5dc473dccadc590e48af16b08a
richearda <richearda1996@gmail.com>
2022-04-14 21:55:35 +0800

    Add layout and functionality to cart
    Added cart layout and add to cart button functionality

--- js/script.js
+++ js/script.js
@@ -1,9 +1,13 @@
 //Variables
-const btnPlus = document.querySelector('#btn-plus');
-const btnMinus = document.querySelector('#btn-minus');
-const productQtyCount = document.querySelector('.count');
-const cart = document.querySelector('.cart');
-const cartIcon = document.querySelector('#cart-icon');
+const btnPlus = document.querySelector("#btn-plus");
+const btnMinus = document.querySelector("#btn-minus");
+const btnAddToCart = document.querySelector("#btn-addtocart");
+const productQtyCount = document.querySelector(".count");
+const cart = document.querySelector(".cart");
+const cartIcon = document.querySelector("#cart-icon");
+const productsInCart = document.querySelector(".products-in-cart");
+const productName = document.querySelector(".product-name").textContent;
+const removeIcon = "";
 let productCount = 1;
 let productPrice = 250.0;
 let productDiscount = 0.5;
@@ -11,22 +15,48 @@ let productDiscount = 0.5;
 let slideIndex = 1;
 
 //Event Listeners
-btnPlus.addEventListener('click', incrementProductQty);
-btnMinus.addEventListener('click', decrementProductQty);
-cartIcon.addEventListener('click', toggleCart);
+btnPlus.addEventListener("click", incrementProductQty);
+btnMinus.addEventListener("click", decrementProductQty);
+cartIcon.addEventListener("click", toggleCart);
+btnAddToCart.addEventListener("click", addToCart);
 
 
+function addToCart(){
+const appendHTML = `
+  <img src="images/image-product-1-thumbnail.jpg" alt="Product Name" id="purchased-product">
+    <div class="product-purchased-info">
+      <p class="product-purchased-name">${productName}</p>
+        <div class="product-total">
+          <span id="product-purchased-price">$${productPrice}</span> x <span id="productQty">${productCount}</span> <span class="purchasedTotal">$${(productPrice * productCount).toFixed(2)}</span>
+        </div>
+      </div>
+  <img src="images/icon-delete.svg" alt="remove to cart" class="remove-product-button">
+`
+
+productsInCart.innerHTML = appendHTML;
+
+if(productsInCart.style.display == 'none')
+{
+  productsInCart.style.display = 'block'; 
+}
+removeIcon = document.querySelector(".remove-product-button");
+removeIcon.addEventListener("click", removeToCart);
+}
+
+function removeToCart() {
+productsInCart.parentNode.removeChild(productsInCart);
+}
 //Functions
 showSlides(slideIndex);
 
 // Next/previous controls
 function plusSlides(n) {
-  showSlides(slideIndex += n);
+  showSlides((slideIndex += n));
 }
 
 // Thumbnail image controls
 function currentSlide(n) {
-  showSlides(slideIndex = n);
+  showSlides((slideIndex = n));
 }
 
 function showSlides(n) {
@@ -39,8 +69,8 @@ function showSlides(n) {
   for (i = 0; i < dots.length; i++) {
     dots[i].className = dots[i].className.replace(" active", "");
   }
-  slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " active";
 }
 
 function incrementProductQty() {
@@ -53,14 +83,14 @@ function decrementProductQty() {
 }
 
 function setProductQtyCount(value) {
-  if ((productCount + value) > 0) {
-      productCount += value;
-      productQtyCount.innerHTML = productCount;
+  if (productCount + value > 0) {
+    productCount += value;
+    productQtyCount.innerHTML = productCount;
   }
   //console.log(value);
 }
 
-function toggleCart(){
+function toggleCart() {
   if (cart.style.display === "none") {
     cart.style.display = "block";
   } else {
